3f4dde4bd46c614c9b070154fc1e7aa842b17da0
Vlan3ik <steammail_38@mail.ru>
2026-06-24 18:27:23 +0300

    Fix media URLs and use git pull deploy

--- deploy/zernoru-pull.sh
+++ deploy/zernoru-pull.sh
@@ -11,11 +11,11 @@ cd "$DEPLOY_DIR"
 if [ ! -d .git ]; then
   git init -b "$BRANCH"
   git remote add "$REMOTE" https://github.com/Vlan3ik/ZernoRU.git
+  git fetch "$REMOTE" "$BRANCH"
+  git checkout -B "$BRANCH" "$REMOTE/$BRANCH"
+  git branch --set-upstream-to="$REMOTE/$BRANCH" "$BRANCH"
 fi
 
-git fetch "$REMOTE" "$BRANCH"
-git checkout -B "$BRANCH" "$REMOTE/$BRANCH"
-git reset --hard "$REMOTE/$BRANCH"
-git clean -fdx
+git pull --ff-only --prune "$REMOTE" "$BRANCH"
 
 docker compose -p "$COMPOSE_PROJECT_NAME" -f docker-compose.prod.yml up -d --build --remove-orphans
